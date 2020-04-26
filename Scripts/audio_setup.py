e79f016b356a6796820d655826782fa11bd04841
HankDeTank05 <henryhankholman@gmail.com>
2020-04-25 20:16:36 -0500

    Create audio_setup.py

---
+++ Scripts/audio_setup.py
@@ -0,0 +1,22 @@
+fname = input("audio file name: ")
+
+# find the song on youtube
+
+# download the video as mp3
+
+# save it to \Scripts\MP3s\
+
+# open it for editing in audacity
+
+# get song bpm from user input
+
+# create rhythm track
+
+'''
+USER MUST ALIGN MP3 AND RHYTHM TRACK IN AUDACITY MANUALLY
+'''
+# save the audacity project to \Scripts\Audacity_project_files\
+
+# mute the rhythm track
+
+# export as song.ogg to \Scripts\OGGs\
